7fbbae5e52b85b9a9c3e768cc2f7e3ff3f66bfd9
Velzog <velzogfr@gmail.com>
2018-03-31 17:21:56 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -19,9 +19,34 @@ bot.on('ready',function() {
 bot.login(process.env.TOKEN);
 
 
+bot.on("guildMemberAdd", member => {
+    member.guild.channels.find("name","welcome").send(`Bienvenue ${member}`);
+
+
+})
+bot.on("guildMemberRemove", member => {
+    member.guild.channels.find("name","welcome").send(`${member} vient de quitter`);
+})
+
+bot.on('guildMemberAdd', member => {
+    var role = member.guild.roles.find('name','Membres');
+    member.addRole(role);
+})
+
 bot.on('message', message => {
     if(message.content === prefix + "help"){
-        message.channel.send('Liste des commandes : \n - ?help \n - ?xp');
+        message.channel.send('Liste des commandes : \n - ?help \n - ?xp \n - ?infodiscord');
+    }
+
+    if(message.content === prefix + "infodiscord"){
+        var embed1 = new Discord.RichEmbed()
+        .setDescription("Information du Discord")
+        .addField("Nom du discord", message.guild.name)
+        .addField("Créer le", message.guild.createdAt)
+        .addField("Tu as rejoins le", message.member.joinedAt)
+        .addField("Utilisateurs sur le discord", message.guild.memberCount)
+        .setColor("0x0000FF")
+        message.channel.sendEmbed(embed1);
     }
 
     if(message.content === "Salut"){
